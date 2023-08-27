7eb840a8b02f8d9858275bdabb94422e37249366
Siftos <adrian.sodergard@gmail.com>
2023-08-27 21:01:13 +0300

    added comments

--- open.go
+++ open.go
@@ -6,6 +6,16 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+//Initiates a new table in the database. The table should be a struct with json tags
+//to be able to easily unmarshal the content later.
+//Example struct to give as table:
+/*
+type User struct {
+	id        int     `json:"id"` -- mandatory field to have in every struct
+	username  string  `json:"username"`
+	password  string  `json:"password"`
+}
+*/
 func Open(databasefile string) *sql.DB {
 	// Open a database connection
 	db, err := sql.Open("sqlite3", databasefile)
